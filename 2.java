5a361873f166f36248f78f75387b00442e764f2c
etunagar@gmil.com <email@gamil.co>
2026-02-25 20:32:58 +0530

    update 2.java

--- 2.java
+++ 2.java
@@ -1,7 +1,7 @@
 bncoewkwncoewnvioewnvioew
 wvnewonvoewinvwpwnvowinv
 nwvoibnwvoibnwibn
-<<<<<<< HEAD
+
 
 wlncwbvwvb
 =======
@@ -9,4 +9,4 @@ qcewuivcuiewvcuioewohvc
   wbncuiewuvuiewgvc
   wvcbvewivuiewvuiew
   wbvcuiewvuibewiuvcw
->>>>>>> 69d9a3f96717ed982bef848a5e7a3d75c4be879c
+
